5fca42017a59c412051398e46aaf205edf393eec
Ajay Singh <ajay87891@gmail.com>
2023-09-15 21:47:08 +0530

    project initialized

---
+++ Components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <aside className='h-screen'>
+        <nav className="h-full flex flex-col bg-bg border-r shadow-sm">
+            <div className=""></div>
+        </nav>
+      
+    </aside>
+  )
+}
+
+export default Sidebar

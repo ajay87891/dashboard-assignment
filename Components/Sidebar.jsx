3dad496efa06ba94dd6a41243c9a59146a617dcf
Ajay Singh <ajay87891@gmail.com>
2023-09-17 15:38:27 +0530

    sidebar

--- Components/Sidebar.jsx
+++ Components/Sidebar.jsx
@@ -7,6 +7,11 @@ import {
   XIcon,
 } from '@heroicons/react/outline'
 
+import bell from './../public/icons/bell.png'
+import office from './../public/icons/office.png'
+import checkbox from './../public/icons/checkbox.png'
+import vl from './../public/icons/vl.png'
+
 
 const navigation = [
   { name: 'Dashboard', href: '#', icon: icons.dashboard, current: false },
@@ -34,7 +39,7 @@ export default function Sidebar({sidebarOpen,setSidebarOpen}) {
       <div className=''>
         {/* {mobile sidebar} */}
         <Transition.Root show={sidebarOpen} as={Fragment}>
-          <Dialog as="div" className="fixed inset-0 flex z-40 md:hidden" onClose={setSidebarOpen}>
+          <Dialog as="div" className="fixed inset-0 flex z-40 lg:hidden" onClose={setSidebarOpen}>
             <Transition.Child
               as={Fragment}
               enter="transition-opacity ease-linear duration-300"
@@ -56,7 +61,7 @@ export default function Sidebar({sidebarOpen,setSidebarOpen}) {
               leaveTo="-translate-x-full"
             >
               
-              <div className="relative flex-1 flex flex-col max-w-xs w-full bg-bg">
+              <div className="relative flex-1 flex flex-col max-w-xs w-full px-3 bg-bg">
                 <Transition.Child
                   as={Fragment}
                   enter="ease-in-out duration-300"
@@ -66,7 +71,7 @@ export default function Sidebar({sidebarOpen,setSidebarOpen}) {
                   leaveFrom="opacity-100"
                   leaveTo="opacity-0"
                 >
-                  <div className="absolute top-0 right-0 -mr-12 pt-2">
+                  <div className="absolute top-0 right-0  pt-2">
                     <button
                       type="button"
                       className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
@@ -77,12 +82,44 @@ export default function Sidebar({sidebarOpen,setSidebarOpen}) {
                     </button>
                   </div>
                 </Transition.Child>
-                <div className="flex-1 h-8 pt-5 pb-4 overflow-y-auto no-scrollbar">
-                  <div className="flex items-center justify-center">
-                    <h2 className='text-white'>
+                <div className="flex items-center justify-start py-3">
+                    <h2 className='text-white text-[18px] px-2'>
                     weframetech
                     </h2>
                   </div>
+                  <div className="flex-1 h-8 pt-5 pb-4 overflow-y-auto no-scrollbar">
+
+                  <div className="relative ">
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none my-2">
+        <svg className="w-4 h-4 text-gray-500  dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+        </svg>
+        <span className="sr-only">Search icon</span>
+      </div>
+      <input type="text" id="search-navbar" className="block  lg:w-[20vw] p-4 py-2 pl-10 text-sm bg-secondary rounded-3xl xl:text-base lg:text-sm" placeholder="Search here"/>
+    </div>
+
+
+
+                  <ul className='flex  p-4  space-x-8'>
+      <li > <div className='relative'> <img src={bell.src} alt="" className='w-5 h-5 xl:w-8 xl:h-8'/> 
+      <span className="absolute bg-[#5ECFFF] text-white font-bold w-4 h-4 xl:h-5 xl:w-5 text-xs  rounded-full -top-2 -right-2 xl:-top-3 xl:-right-3 flex xl:text-xs text-[7px] items-center justify-center">12</span></div>
+      </li>
+      <li > <div className='relative'> <img src={vl.src} alt="" className='w-5 h-5 xl:w-8 xl:h-8'/> 
+      <span className="absolute bg-[#5ECFFF] text-white font-bold w-4 h-4 xl:h-5 xl:w-5  rounded-full -top-2 -right-2 xl:-top-3 xl:-right-3 xl:text-xs text-[7px]  flex items-center justify-center">12</span></div>
+      </li>
+      
+      <li > <div className='relative'> <img src={checkbox.src} alt="" className='w-5 h-5 xl:w-8 xl:h-8'/> 
+      <span className="absolute bg-[#5ECFFF] text-white font-bold w-4 h-4 xl:h-5 xl:w-5 xl:text-xs text-[7px]  rounded-full -top-2 -right-2 xl:-top-3 xl:-right-3  flex items-center justify-center">12</span></div>
+      </li>
+      <li > <div className='relative'> <img src={office.src} alt="" className='w-5 h-5 xl:w-8 xl:h-8'/> 
+      <span className="absolute bg-[#E328AF] text-white font-bold w-4 h-4 xl:h-5 xl:w-5 xl:text-xs text-[7px]  rounded-full -top-2 -right-2 xl:-top-3 xl:-right-3  flex items-center justify-center">!</span></div>
+      </li>
+     
+     
+    </ul>
+                
+                  
                   <nav className="mt-5 px-2 space-y-1">
                     {navigation.map((item) => (
                       <a
@@ -127,15 +164,15 @@ export default function Sidebar({sidebarOpen,setSidebarOpen}) {
         <div className="hidden lg:flex md:w-52 lg:flex-col lg:fixed lg:inset-y-0 ">
          
           <div className="flex-1 flex flex-col min-h-0 bg-bg">
-            <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto no-scrollbar">
-              <div className="flex items-center justify-center p-4">
+            <div className="flex-1 flex flex-col  pb-4 overflow-y-auto no-scrollbar">
+              <div className="flex items-center justify-center pt-4">
 
 
-                <h2 className='text-white lg:text-lg xl:text-xl 2xl:text-2xl font-normal'>weframetech</h2>
+                <h2 className='text-white lg:text-sm xl:text-lg 2xl:text-2xl font-normal'>weframetech</h2>
               </div>
               <div>
               
-              <nav className="mt-5 flex-1 px-2 space-y-1">
+              <nav className="mt-4 xl:mt-5 flex-1 px-2 space-y-1">
               <h3 className='text-white font-openSans  xl:text-sm px-2 2xl:text-lg'>MAIN MENU</h3>
 
 
